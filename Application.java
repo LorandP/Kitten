7176b7daf28a67b8f9952f135a88ba814c49afe5
Lori <palfalvi.lorand@gmail.com>
2017-02-03 10:23:31 +0200

    Changed the loop from while to for.

--- Application.java
+++ Application.java
@@ -3,26 +3,27 @@
  */
 public class Application
 {
-
+    /**
+     * This method returns a string from which a specific character was removed from.
+     * @param str the original string.
+     * @param index the index that specifies which character to be removed from the string.
+     */
     private void missingChar(String str, int index)
     {
-        int i = 0;
-
         if (str != null && !str.isEmpty())
         {
-           while (i <= str.length()-1)
-           {
-               if (i == index)
-                   i++;
-               System.out.print(str.charAt(i));
-               i++;
-           }
+            for (int counter = 0; counter <= str.length()-1;counter++)
+            {
+                if (counter == index)
+                    counter++;
+                System.out.print(str.charAt(counter));
+            }
         }
     }
     public static void main (String []args)
     {
         Application application = new Application();
 
-        application.missingChar("kitten",4);
+        application.missingChar("kitten",0);
     }
 }
